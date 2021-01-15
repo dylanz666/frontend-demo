af11b7b4150c94784c53aaf864d83f2cde862eb9
diren <diren@gaoding.com>
2021-01-15 12:31:01 +0800

    set up project

---
+++ js/math.js
@@ -0,0 +1,5 @@
+function add() {
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = parseInt(num1) + parseInt(num2);
+}
